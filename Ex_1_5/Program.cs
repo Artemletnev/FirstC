4835540a8f3fcac700f02971fc46b4652596f210
Artem <letnev2012@gmail.com>
2022-06-23 18:54:56 +0300

    add programm hello Mari

---
+++ Ex_1_5/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Enter your name:");
+string username = Console.ReadLine();
+
+if(username .ToLower()== "mari")
+{
+    Console.WriteLine("Is it HER!!!");
+}
+else 
+{
+    Console.Write("Hello,");
+    Console.WriteLine(username);
+}
